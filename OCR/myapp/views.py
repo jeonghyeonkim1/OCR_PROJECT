03daf1e7015f4dd2968caf35b5c15259347f95ba
dongwan123 <102333735+dongwan123@users.noreply.github.com>
2022-06-25 15:30:35 +0900

    dw0625_2

--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import os
+from PIL import Image
+import pytesseract
 
 # Create your views here.
 
 
 
-def ocr(req):
-    return render(req, 'base.html')
+def ocr(request):
+    context = {}
+    if 'uploadfile' in request.FILES:
+        uploadfile = request.FILES.get('uploadfile', '')
+
+        if uploadfile != '':
+            name_old = uploadfile.name
+
+            fs = FileSystemStorage(location='static/source')
+            imgname = fs.save(f'src-{name_old}', uploadfile)
+            imgfile = Image.open(f'./static/source/{imgname}')
+            resulttext = pytesseract.image_to_string(imgfile, lang='kor')
+
+    context['imgname'] = imgname
+    context['resulttext'] = resulttext.replace(" ", "")
+
+    return render(request, 'coocr_upload.html', context)
