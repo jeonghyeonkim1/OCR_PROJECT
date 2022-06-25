2f3e27aec612683b2e43695d52e0f9b213b5b19d
jeonghyeonkim <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-06-25 16:35:25 +0900

    jh

--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
-import os
-from PIL import Image
+from functools import reduce
 import pytesseract
+import sys
+import os
+import cv2
+import numpy as np
+import pytesseract
+import re
+import time
 
-# Create your views here.
 
-
-
-def ocr(request):
+def ocr_uploader(req):
     context = {}
-    if 'uploadfile' in request.FILES:
-        uploadfile = request.FILES.get('uploadfile', '')
+    if 'uploadfile' in req.FILES:
+        uploadfile = req.FILES.get('uploadfile', '')
 
         if uploadfile != '':
-            name_old = uploadfile.name
+            cam = cv2.VideoCapture(uploadfile)
+            fcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+            out = cv2.VideoWriter('/static/out/output.avi', fcc, fps, (width, height))
 
-            fs = FileSystemStorage(location='static/source')
-            imgname = fs.save(f'src-{name_old}', uploadfile)
-            imgfile = Image.open(f'./static/source/{imgname}')
-            resulttext = pytesseract.image_to_string(imgfile, lang='kor')
+            while (cap.isOpened()):
+                ret, frame = cap.read()
+                w, h, fps = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), cap.get(cv2.CAP_PROP_FPS)
 
-    context['imgname'] = imgname
-    context['resulttext'] = resulttext.replace(" ", "")
+                if ret:
+                    out.write(frame)
+                    
+                else:
+                    print("Fail to read frame!")
+                    break
+                
+            out.release()
+            cap.release()
 
-    return render(request, 'coocr_upload.html', context)
+    return render(request, 'base.html', context)
+    
+def ocr(req):
+    return render(request, 'base.html')
