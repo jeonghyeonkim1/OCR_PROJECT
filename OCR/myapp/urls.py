9c580e0196737f23648009ea777086e1ad7da258
jjangseo1 <93521234+seoheejj1@users.noreply.github.com>
2022-06-26 20:18:06 +0900

    sh1

--- OCR/myapp/urls.py
+++ OCR/myapp/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path, include
+from torch import view_as_complex
 from . import views
 
 app_name = 'ocr'
@@ -7,4 +8,6 @@ app_name = 'ocr'
 urlpatterns = [
     path('enter/', views.enter, name='enter'),
     path('enter_call/', views.enter_call, name='enter_call'),
+    path('scan/', views.scan, name='scan'),
+    path('scan/mycam', views.mycam, name='mycam'),
 ]
--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from string import whitespace
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+from functools import reduce
+import pytesseract
+import sys
 import os
 import cv2
+import numpy as np
+import pytesseract
+import re
+import time
+from django.views.decorators import gzip
+import threading
+
 
 
 def enter(request):
@@ -62,3 +75,138 @@ def enter_call(request):
         cv2.destroyAllWindows()
 
         return JsonResponse({'success': '영상 업로드 완료!'})
+
+
+# def ocr(request):
+#     context = {}
+#     if 'video_url' in request.POST:
+        
+#         os.mkdir('./OCR/static/out') if not os.path.exists('./OCR/static/out') else print('이미 폴더가 존재합니다.')
+
+#         cap = cv2.VideoCapture(request.POST['video_url'])
+
+#         w, h, fps = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(
+#             cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), cap.get(cv2.CAP_PROP_FPS)
+#         fcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+#         out = cv2.VideoWriter('./OCR/static/out/output.mp4',
+#                               fcc, fps, (w, h))
+
+#         context = {'route': './OCR/static/out/output.mp4'}
+        
+#         while True:
+#             retval, frame = cap.read()
+#             if retval:
+#                 out.write(frame)
+
+#             if not retval:
+#                 break
+
+#             cv2.imshow('video_name', frame)
+
+#             if cv2.waitKey(50) == 27:
+                
+#                 break
+            
+#         out.release()
+#         cap.release()
+
+#         cv2.destroyAllWindows()
+
+#     return render(request, 'base.html', context)
+
+
+# def video(request):
+#     context = {'route': './OCR/static/out/output.mp4'}
+
+#     model = r'D:\DevRoot\dataset\ComputerVision\dnnface\res10_300x300_ssd_iter_140000_fp16.caffemodel'
+#     config = r'D:\DevRoot\dataset\ComputerVision\dnnface\deploy.prototxt'
+
+#     cap = cv2.VideoCapture('./OCR/static/out/output.mp4')
+
+
+#     net = cv2.dnn.readNet(model, config)
+#     if net.empty():
+#         print('Net open failed!')
+#         exit()
+
+#     if not cap.isOpened():
+#         print('video open failed')
+#         sys.exit()
+
+#     while True:
+#         _, frame = cap.read()
+#         if frame is None:
+#             break
+
+#         blob = cv2.dnn.blobFromImage(frame, 1, (300, 300), (104, 177, 123))
+#         net.setInput(blob)
+#         detect = net.forward()
+
+#         detect = detect[0, 0, :, :]
+#         (h, w) = frame.shape[:2]
+
+#         for i in range(detect.shape[0]):
+#             confidence = detect[i, 2]
+#             if confidence < 0.5:
+#                 break
+
+#             x1 = int(detect[i, 3] * w)
+#             y1 = int(detect[i, 4] * h)
+#             x2 = int(detect[i, 5] * w)
+#             y2 = int(detect[i, 6] * h)
+
+#             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0))
+
+#             label = 'Face: %4.3f' % confidence
+#             cv2.putText(frame, label, (x1, y1 - 1),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+#         cv2.imshow('frame', frame)
+
+#         if cv2.waitKey(50) == 27:
+#             break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+#     return render(request, 'base.html', context)
+
+def scan(request):
+    return render(request, 'base2.html')
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def mycam(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        print("에러입니다...")
+        pass
+
+
