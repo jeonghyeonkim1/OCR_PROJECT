6fd7f244fce00f4c2011954d4a3b3717362010ed
jeonghyeonkim1 <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-06-26 17:34:40 +0900

    jh

--- OCR/myapp/urls.py
+++ OCR/myapp/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'ocr'
+
 urlpatterns = [
-    path('ocr/', views.ocr),
-    path('ocr/video', views.video),
+    path('enter/', views.enter, name='enter'),
+    path('enter_call/', views.enter_call, name='enter_call'),
 ]
