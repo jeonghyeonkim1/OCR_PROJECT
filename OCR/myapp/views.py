96b6ba742f57d0f2b948608269d8efe1f7d7f8c5
jjangseo1 <93521234+seoheejj1@users.noreply.github.com>
2022-06-27 13:20:19 +0900

    sh2

--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -5,27 +5,13 @@ import cv2
 import pytesseract
 from django.views.decorators import gzip
 import threading
+from myapp.camera import VideoCamera
+from requests import Response
+
 
 def scan(request):
     return render(request, 'base2.html')
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
-
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        image = self.frame
-        _, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
-
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
 
 
 def gen(camera):
@@ -35,11 +21,6 @@ def gen(camera):
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@gzip.gzip_page
 def mycam(request):
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        print("에러입니다...")
-        pass
+    return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
+
