a981b03378b7743916e26a07397ab083510a3e81
Leeholim <86648396+Leeholim@users.noreply.github.com>
2022-06-29 17:39:01 +0900

    Update views.py

--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -27,26 +27,14 @@ class VideoCamera(object):
 
     def get_frame(self):
         
-
-        cntr = 0
         while True:
-            _, image = self.video.read()
-            # cntr += 1
-            # if((cntr%20) == 0):
-            #     imgh, imgw, chan = image.shape
-                # x1,y1,w1,h1 = 0,0,imgh,imgw
-                # imgchar = pytesseract.image_to_string(image, lang='kor')
-                # imgboxes = pytesseract.image_to_boxes(image)
-
-                # for boxes in imgboxes.splitlines():
-                #     boxes = boxes.split()
-                #     x1,y1,w1,h1 = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
-                #     cv2.rectangle(image, (x1, imgh-y1), (w1, imgh-h1), (0,0,0),3)
-                #     cv2.putText(image, boxes[0],(x1,imgh-h1+32), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
-                    
-                
-                # (self.grabbed, self.frame) = self.video.read()
-                # threading.Thread(target=self.update, args=()).start()
+            ret, image = self.video.read()
+            if ret:
+                # cv2.imshow('camera', image)
+                if cv2.waitKey(1) == 13:
+                    cv2.imwrite('photo.jpg', image)
+                    a = cv2.imread('photo.jpg')
+                    cv2.imshow('img', a)
             _, jpeg = cv2.imencode('.jpg', image)
             return jpeg.tobytes()
 
@@ -71,16 +59,4 @@ def mycam(request):
         print("에러입니다...")
         pass
 
-# def gen(camera, camera2):
-#     while True:
-#         frame = camera.get_frame()
-#         frame2 = camera2.get_frame()
-#         yield(b'--frame\r\n'
-#               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-#         yield(b'--frame\r\n'
-#               b'Content-Type: image/jpeg\r\n\r\n' + frame2 + b'\r\n\r\n')
-
-
-# def mycam(request):
-#     return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
 
