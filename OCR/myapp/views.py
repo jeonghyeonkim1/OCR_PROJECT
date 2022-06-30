6b734fb31fe12e6c84ddb1ccbb0bb868cd1943ad
jeonghyeonkim1 <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-06-30 14:52:34 +0900

    jh

--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import StreamingHttpResponse
-from django.shortcuts import render
+from django.http import StreamingHttpResponse, JsonResponse
 import cv2
 import numpy as np
 import pytesseract
@@ -26,29 +25,28 @@ class VideoCamera(object):
         self.video.release()
 
     def get_frame(self):
-        
-
         cntr = 0
-        while True:
-            _, image = self.video.read()
-            # cntr += 1
-            # if((cntr%20) == 0):
-            #     imgh, imgw, chan = image.shape
-                # x1,y1,w1,h1 = 0,0,imgh,imgw
-                # imgchar = pytesseract.image_to_string(image, lang='kor')
-                # imgboxes = pytesseract.image_to_boxes(image)
+        _, image = self.video.read()
+        # cntr += 1
+        # if((cntr%20) == 0):
+        #     imgh, imgw, chan = image.shape
+            # x1,y1,w1,h1 = 0,0,imgh,imgw
+            # imgchar = pytesseract.image_to_string(image, lang='kor')
+            # imgboxes = pytesseract.image_to_boxes(image)
 
-                # for boxes in imgboxes.splitlines():
-                #     boxes = boxes.split()
-                #     x1,y1,w1,h1 = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
-                #     cv2.rectangle(image, (x1, imgh-y1), (w1, imgh-h1), (0,0,0),3)
-                #     cv2.putText(image, boxes[0],(x1,imgh-h1+32), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
-                    
+            # for boxes in imgboxes.splitlines():
+            #     boxes = boxes.split()
+            #     x1,y1,w1,h1 = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
+            #     cv2.rectangle(image, (x1, imgh-y1), (w1, imgh-h1), (0,0,0),3)
+            #     cv2.putText(image, boxes[0],(x1,imgh-h1+32), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
                 
-                # (self.grabbed, self.frame) = self.video.read()
-                # threading.Thread(target=self.update, args=()).start()
-            _, jpeg = cv2.imencode('.jpg', image)
-            return jpeg.tobytes()
+            
+            # (self.grabbed, self.frame) = self.video.read()
+            # threading.Thread(target=self.update, args=()).start()
+
+        _, jpeg = cv2.imencode('.jpg', image)
+
+        return jpeg.tobytes()
 
     def update(self):
         while True:
@@ -57,6 +55,7 @@ class VideoCamera(object):
 
 def gen(camera):
     while True:
+
         frame = camera.get_frame()
         yield(b'--frame\r\n'
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
--- OCR/mycar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'car'
-
-urlpatterns = [
-    path('enter/', views.enter, name='enter'),
-    path('enter_call/', views.enter_call, name='enter_call'),
-]
