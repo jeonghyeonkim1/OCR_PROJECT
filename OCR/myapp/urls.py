31372de20a9c769b7c1cadaecfc9ec100ac773de
jeonghyeonkim1 <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-07-05 22:51:46 +0900

    jh

--- OCR/myapp/urls.py
+++ OCR/myapp/urls.py
@@ -6,7 +6,6 @@ app_name = 'ocr'
 urlpatterns = [
     path('', views.home, name='home'),
     path('scan/', views.scan, name='scan'),
-    path('scan/mycam/', views.mycam, name='mycam'),
     path('scan/cam/', views.cam, name='cam'),
     path('scan/get_cam', views.get_cam, name='get_cam'),
     path('scan/recommend/', views.recommend, name='recommend'),
--- OCR/myapp/views.py
+++ OCR/myapp/views.py
@@ -1,7 +1,10 @@
 from ast import While
 from multiprocessing.connection import wait
+from turtle import window_height, window_width
 from django.shortcuts import render
 from django.http import JsonResponse
+import os
+import time
 import cv2
 import numpy as np
 import pytesseract
@@ -18,8 +21,21 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 import roboflow
 from roboflow import Roboflow
+from tkinter import *
 
-res = {}
+root = Tk()
+
+monitor_height = root.winfo_screenheight()
+monitor_width = root.winfo_screenwidth()
+
+rf = Roboflow(api_key="BzyHkzKOlMSJcspr3EH2")
+workspace = rf.workspace()
+workspace.name
+workspace.url
+workspace.projects()
+
+project = rf.workspace("seoheejang").project("chess-sample-m5hzq")
+model = project.version(6).model
 
 def home(request):
     return render(request, 'home.html')
@@ -28,51 +44,83 @@ def scan(request):
     return render(request, 'base2.html')
 
 def get_cam(request):
-    capture = cv2.VideoCapture(0)
+    file_path = './static/contour_list'
+    os.mkdir(file_path) if not os.path.exists(file_path) else ''
+
+    capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     print('camera open failed') if not capture.isOpened() else ''
+
+    # cv2.resizeWindow('qwe', monitor_width, monitor_height)
+    # cv2.moveWindow('qwe', 0, 0)
 
     while 1:
         ret, frame = capture.read()
         if not ret:
             break
 
+        cv2.imwrite('./static/data/hi.jpg', frame)
+
+        prediction = model.predict('./static/data/hi.jpg').json()['predictions']
+
+        if len(prediction) > 0:
+            prediction = prediction[0]
+
+            if prediction['class'] == 'book' and prediction['confidence'] >= 0.6:
+                x, y, w, h = int(prediction['x']), int(prediction['y']), int(prediction['width']), int(prediction['height'])
+
+                cv2.rectangle(
+                    frame,
+                    pt1=(x - int(w/2), y - int(h/2)),
+                    pt2=(x + int(w/2), y + int(h/2)),
+                    color=(0, 255, 255),
+                    thickness=2
+                )
+
+                cv2.putText(
+                    frame,
+                    text = f"{prediction['class']} {round(prediction['confidence'] * 100, 1)}%",
+                    org = (x - int(w/2), y - int(h/2) - 20),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1,
+                    color = (0, 255, 255),
+                    thickness=2
+                )
+
         cv2.imshow('qwe', frame)
-        
+
         if cv2.waitKey(1) == 13:
+            if prediction['class'] == 'book' and prediction['confidence'] >= 0.6:
+                contour = {
+                    'lu': (x - int(w/2), y - int(h/2)),
+                    'ru': (x + int(w/2), y - int(h/2)),
+                    'rb': (x + int(w/2), y + int(h/2)),
+                    'lb': (x - int(w/2), y + int(h/2)),
+                    'width': prediction['width'],
+                    'height': prediction['height'],
+                }
+
+                srcQuad = np.array([
+                    contour['lu'], contour['ru'], 
+                    contour['rb'], contour['lb'],
+                ]).astype(np.float32)
+
+                dstQuad = np.array([
+                    [0, 0], [contour['width'], 0],
+                    [contour['width'], contour['height']], [0, contour['height']],
+                ]).astype(np.float32)
+
+                pers = cv2.getPerspectiveTransform(srcQuad, dstQuad)
+                dst = cv2.warpPerspective(frame, pers, (w, h))
+
+                cv2.imwrite(os.path.join(file_path, f'contour.jpg'), dst)
+
+                cv2.imshow('qwe', cv2.imread('./static/contour_list/contour.jpg'))
+
+                cv2.waitKey()
+
             break
 
     capture.release() 
-    cv2.destroyAllWindows()
-    return JsonResponse({})
-
-
-def mycam(request):  
-    camera = cv2.VideoCapture(0)
-    print('camera open failed') if not camera.isOpened() else ''
-    # camera = cv2.VideoCapture(cv2.CAP_DSHOW+0)
-
-    rf = Roboflow(api_key="BzyHkzKOlMSJcspr3EH2")
-    workspace = rf.workspace()
-    workspace.name
-    workspace.url
-    workspace.projects()
-
-    project = rf.workspace("seoheejang").project("chess-sample-m5hzq")
-    model = project.version(6).model
-
-
-    
-    ret , img = camera.read()
-
-    # if not ret: break
-    # prediction = model.predict(img)
-    # print(prediction)
-    cv2.imshow('sdf', img)
-    if cv2.waitKey(0) == 27:
-        pass
-        # break 
-        
-    camera.release()
     cv2.destroyAllWindows()
     return JsonResponse({})
 
